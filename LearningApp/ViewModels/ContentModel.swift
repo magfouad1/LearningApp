11ff98e2fe353f9db406f3e1fce478902e4cde50
Magfouad <maged.hm@gmail.com>
2022-07-19 17:08:05 -0400

    Add data and parsing it

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,50 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        
+        getLocaData()
+    }
+    
+    func getLocaData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+            
+        }
+        
+        catch {
+            
+            print("Error reading json file")
+            
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+
+            
+            self.styleData = try Data(contentsOf: styleUrl!)
+        }
+        
+        catch {
+            print("Couldn't parse style data")
+        }
+        
+        
+    }
+    
 }
